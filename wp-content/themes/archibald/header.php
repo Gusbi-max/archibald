7f5c542b7cd3781a75aefca299c8c04b763efc84
Gusbi-max <bigot.max@gmail.com>
2021-03-15 10:14:09 +0100

    update content

--- wp-content/themes/archibald/header.php
+++ wp-content/themes/archibald/header.php
@@ -4,7 +4,24 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta name="robots" content="all" />
-  <?php wp_head() ?>
-  <title>Document</title>
+  <?php
+    wp_head(); 
+  ?>
 </head>
 <body>
+  <header>
+    <div class="line-blue"></div>
+    <div class="line-red"></div>
+    <div class="navigation">
+      <a href="<?= home_url('/') ?>">
+        <img class="logo" src="<?= get_template_directory_uri() . '/assets/images/logo_small.png' ?>" alt="Logo Archibald">
+      </a>
+      <div class="menu">
+        <div class="menu-burger">
+          <span class="burger-line line-top"></span>
+          <span class="burger-line line-middle"></span>
+          <span class="burger-line line-bottom"></span>
+        </div>
+      </div>
+    </div>
+  </header>
