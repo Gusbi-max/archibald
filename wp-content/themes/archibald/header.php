5257c984a5aa47da4597fd5062670bc87ea33318
Gusbi-max <bigot.max@gmail.com>
2021-03-19 14:41:16 +0100

    page contact responsive

--- wp-content/themes/archibald/header.php
+++ wp-content/themes/archibald/header.php
@@ -10,20 +10,7 @@
 </head>
 <body>
   <header>
-    <div class="line-blue"></div>
-    <div class="line-red"></div>
-    <div class="navigation">
-      <a href="<?= home_url('/') ?>">
-        <img class="logo" src="<?= get_template_directory_uri() . '/assets/images/logo_small.png' ?>" alt="Logo Archibald">
-      </a>
-      <div class="menu">
-        <div class="menu-burger">
-          <span class="burger-line line-top"></span>
-          <span class="burger-line line-middle"></span>
-          <span class="burger-line line-bottom"></span>
-        </div>
-      </div>
-    </div>
+    
   </header>
 
   <div class="container">
