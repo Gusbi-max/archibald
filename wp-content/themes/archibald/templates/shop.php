7de07fcfb2b00ecc01e85ac3a464bcf9948fb6e8
Frank Bergé <frank.brg@icloud.com>
2021-03-21 19:55:50 +0100

    feat : add template of product

--- wp-content/themes/archibald/templates/shop.php
+++ wp-content/themes/archibald/templates/shop.php
@@ -4,24 +4,31 @@ Template Name: Boutique
 Template Post Type: page
 */
 ?>
-
+<?php get_header(); ?>
 <?php
 $query = new WP_Query([
   'post_type' => 'product'
 ]);
 ?>
+<div class="product-container">
 
-<?php if ($query->have_posts()): while ($query->have_posts()): $query->the_post(); ?>
+  <?php if ($query->have_posts()): while ($query->have_posts()): $query->the_post(); ?>
+  <div class="product-item">
+    <?php global $product; ?>
+    <div class="product-item-image">
+      <img src="<?php echo wp_get_attachment_url( $product->get_image_id() ); ?>" />
+    </div>
+    <?php
+      echo the_title();
+      echo woocommerce_template_single_price();
+    ?>
+    <div class="product-item-add-block">
+      <?php
+      echo woocommerce_template_single_add_to_cart();
+      ?>
+    </div>
+  </div>
 
-  <a href="<?= the_permalink() ?>">
-    <?= the_title(); ?>
-  </a>
-
-<?php
-  echo woocommerce_template_single_excerpt();
-  echo woocommerce_template_single_price();
-  echo woocommerce_template_single_add_to_cart();
-?>
-
-
-<?php endwhile; endif; ?>
+  <?php endwhile; endif; ?>
+</div>
+<?php get_footer(); ?>
