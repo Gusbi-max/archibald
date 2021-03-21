967df9e5412c6cd9d6a184ae8348a02283a17413
Aurelien <aurelien.giraud2008@gmail.com>
2021-03-21 17:46:04 +0100

    Footer & header responsive without animation

--- wp-content/themes/archibald/footer.php
+++ wp-content/themes/archibald/footer.php
@@ -1,4 +1,47 @@
 <?php wp_footer(); ?>
   </div>
+  <footer>
+	<div class="footer-section">
+		<div id="first-column">
+			<img class="footer-logo" src="<?= get_template_directory_uri() . '/assets/images/logo_archibald.png' ?>" alt="archibald">
+			<p class="footer-text text-white">
+				Sed ut perspiciatis unde omnis iste natus error sit voluptatem<br> 
+				accusantium doloremque laudantium, totam rem aperiam, eaque<br> 
+				ipsa quae ab illo inventore veritatis et quasi architecto beatae<br> 
+				vitae dicta sunt explicabo.
+			</p>
+		</div>
+		<div id="second-column">
+			<h3 class="text-white">LIENS UTILES</h3>
+			<div class="line-title"></div>
+			<a href="#" class="footer-link text-white">Archibald</a>
+			<a href="#" class="footer-link text-white">Tonic de Distillerie</a>
+			<a href="#" class="footer-link text-white">Engagé et Responsable</a>
+			<a href="#" class="footer-link text-white">Dégustation</a>
+		</div>
+		<div id="third-column">
+			<a href="#" class="footer-link text-white">Contact</a>
+			<a href="#" class="footer-link text-white">Blogs et News</a>
+			<a href="#" class="footer-link text-white">E-Shop</a>
+			<a href="#" class="footer-link text-white">Nos CGV</a>
+		</div>
+		<div id="fourth-column">
+			<h3 class="text-white">LIVRAISON</h3>
+			<div class="line-title"></div>
+			<p class="footer-text text-white">
+				Livraison standard en 3 à 5 jours ouvrées à<br>
+				6,90€ TTC,  offerts à partir de 60€ d’achat*<br>
+				Livraison express en 1 jour à 14,90€ TTC*<br>
+				Prix et Délais de livraison par pays Paiement 100% sécurisé
+			</p>
+		</div>
+	</div>
+	<div class="footer-section">
+		<div>
+			<div class="line-grey"></div>
+			<p class="text-white text-center">© 2021, Archibald Tonic</p>
+		</div>
+	</div>
+  </footer>
 </body>
 </html>
