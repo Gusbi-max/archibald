ead5d40d0f33f165946808624e0b28f5728968de
Frank Bergé <frank.brg@icloud.com>
2021-03-27 19:20:30 +0100

    feat (shop) : added resposive

--- wp-content/themes/archibald/templates/shop.php
+++ wp-content/themes/archibald/templates/shop.php
@@ -10,70 +10,82 @@ $query = new WP_Query([
   'post_type' => 'product',
   'product_cat' => 'archibald'
 ]);
+
 ?>
+
 <img class="background-img" src="<?= get_template_directory_uri() . '/assets/images/backdrop-shop.png' ?>" alt="">
 <div class="input-container">
-  <select name="" id="select">
+  <select class="orderby" name="" id="">
       <option value="">Trier par :</option>
       <option value="dog">test1</option>
       <option value="cat">test2</option>
   </select>
   <?php get_product_search_form() ?>
-  </div>
+</div>
 <div class="shop-container">
 <div class="filter-container">
+  <h2>FILTRES</h2> 
   <div class="filter-category">
     <h3 class="category-title">test</h3>
-    <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
+    <div class="line"></div>
+    <div class="filter-item ">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
       <label for="test1">test1</label>
     </div>
     <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
-      <label for="test1">test1</label>
-    </div>
-    <h3 class="category-title">test</h3>
-    <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
-      <label for="test1">test1</label>
-    </div>
-    <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
-      <label for="test1">test1</label>
-    </div>
-    <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
       <label for="test1">test1</label>
     </div>
     <h3 class="category-title">test</h3>
+    <div class="line"></div>
     <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
       <label for="test1">test1</label>
     </div>
     <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
       <label for="test1">test1</label>
     </div>
     <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
-      <label for="test1">test1</label>
-    </div>
-    <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
       <label for="test1">test1</label>
     </div>
     <h3 class="category-title">test</h3>
+    <div class="line"></div>
     <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
       <label for="test1">test1</label>
     </div>
     <div class="filter-item">
-      <input type="checkbox" name="test1" id="">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
+      <label for="test1">test1</label>
+    </div>
+    <div class="filter-item">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
+      <label for="test1">test1</label>
+    </div>
+    <div class="filter-item">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
+      <label for="test1">test1</label>
+    </div>
+    <h3 class="category-title">test</h3>
+    <div class="line"></div>
+    <div class="filter-item">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
+      <label for="test1">test1</label>
+    </div>
+    <div class="filter-item">
+      <input class="default-input checkbox" type="checkbox" name="test1" id="">
       <label for="test1">test1</label>
     </div>
   </div>
 </div>
+
 <div class="products-container">
+<div class="filter-header">
+<img class="filter-icon" src="<?= get_template_directory_uri() . '/assets/images/filter.svg' ?>" alt="">
+<label class="path" >Archibald > Boutique</label> 
+</div>
   <h1>Archibald Tonic</h1>
   <div class="product-container">
 
