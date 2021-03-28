d5b17fa208c3f73d16f06f484035859ff7c31d20
Aurelien <aurelien.giraud2008@gmail.com>
2021-03-28 15:22:55 +0200

    Coordonnées

--- wp-content/themes/archibald/functions.php
+++ wp-content/themes/archibald/functions.php
@@ -22,5 +22,85 @@ function montheme_register_assets() {
 add_action('after_setup_theme', 'montheme_supports');
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
 
+/* Coordonnées ---------------------------------------------------------------------------------------------------------- */
+
+// Modification de l'ordre des champs de la livraison
+add_filter( 'woocommerce_default_address_fields', 'shipping_reorder_checkout_fields' );
+
+function shipping_reorder_checkout_fields( $fields ) {
+
+ $fields['first_name']['priority'] = 10;
+ $fields['last_name']['priority'] = 20;
+ $fields['address_1']['priority'] = 30;
+ $fields['address_2']['priority'] = 35;
+ $fields['postcode']['priority'] = 40;
+ $fields['city']['priority'] = 50;
+ $fields['country']['priority'] = 60;
+ $fields['state']['priority'] = 70;
+ $fields['company']['priority'] = 85;
+
+ return $fields;
+}
+
+// Modification de l'ordre des champs de la facturation
+add_filter( 'woocommerce_billing_fields', 'billing_reorder_checkout_fields' );
+ 
+function billing_reorder_checkout_fields( $fields ) {
+
+    $fields['billing_email']['priority'] = 5;
+    $fields['billing_phone']['priority'] = 80;
+
+    return $fields;
+}
+
+// Suppression des champs inutiles 
+add_filter( 'woocommerce_enable_order_notes_field', '__return_false' );
+add_filter( 'woocommerce_checkout_fields', 'bbloomer_remove_woo_checkout_fields' );
+  
+function bbloomer_remove_woo_checkout_fields( $fields ) {
+  unset($fields['shipping']['shipping_company']);
+  unset($fields['shipping']['shipping_address_2']);
+  unset($fields['billing']['billing_company']);
+  unset($fields['billing']['billing_address_2']);
+    
+    return $fields;
+}
+
+// Personnalisation des champs
+add_filter( 'woocommerce_checkout_fields', 'bbloomer_rename_woo_checkout_fields' );
+  
+function bbloomer_rename_woo_checkout_fields( $fields ) {
+  
+    $fields['billing']['billing_email']['placeholder'] = 'Email';
+    $fields['billing']['billing_email']['label'] = '';
+    $fields['billing']['billing_first_name']['placeholder'] = 'Prénom';
+    $fields['billing']['billing_first_name']['label'] = '';
+    $fields['billing']['billing_last_name']['placeholder'] = 'Nom';
+    $fields['billing']['billing_last_name']['label'] = '';
+    $fields['billing']['billing_address_1']['placeholder'] = 'Adresse';
+    $fields['billing']['billing_address_1']['label'] = '';
+    $fields['billing']['billing_city']['placeholder'] = 'Ville';
+    $fields['billing']['billing_city']['label'] = '';
+    $fields['billing']['billing_country']['label'] = '';
+    $fields['billing']['billing_postcode']['placeholder'] = 'Code Postal';
+    $fields['billing']['billing_postcode']['label'] = '';
+    $fields['billing']['billing_phone']['placeholder'] = 'Téléphone';
+    $fields['billing']['billing_phone']['label'] = '';
+    
+    $fields['shipping']['shipping_first_name']['placeholder'] = 'Prénom';
+    $fields['shipping']['shipping_first_name']['label'] = '';
+    $fields['shipping']['shipping_last_name']['placeholder'] = 'Nom';
+    $fields['shipping']['shipping_last_name']['label'] = '';
+    $fields['shipping']['shipping_address_1']['placeholder'] = 'Adresse';
+    $fields['shipping']['shipping_address_1']['label'] = '';
+    $fields['shipping']['shipping_city']['placeholder'] = 'Ville';
+    $fields['shipping']['shipping_city']['label'] = '';
+    $fields['shipping']['shipping_country']['label'] = '';
+    $fields['shipping']['shipping_postcode']['placeholder'] = 'Code Postal';
+    $fields['shipping']['shipping_postcode']['label'] = '';
+  
+  return $fields;
+}
+
 
 
--- wp-content/themes/archibald/templates/page-contact-information.php
+++ wp-content/themes/archibald/templates/page-contact-information.php
@@ -1,11 +1,40 @@
 <?php
 /*
-Template Name: Page Validation Commande
+Template Name: Page Coordonnées
 */
 ?>
 
 <?php get_header(); ?>
 
+<form name="checkout" method="post" class="checkout woocommerce-checkout" action="<?php echo esc_url( wc_get_checkout_url() ); ?>" enctype="multipart/form-data">
+    <div class="contact-information">
+        <div class="order-tunnel">
+            <p class="title-tunnel tunnel-step">Coordonnées</p>
+            <div class="line-tunnel"></div>
+            <p class="title-tunnel">Livraison</p>
+            <div class="line-tunnel"></div>
+            <p class="title-tunnel">Paiement</p>
+        </div>
+        <div class="shipping-and-billing">
+            <h1 class="title-page-ci">Coordonnées</h1>
+            <?php do_action('woocommerce_before_checkout_form'); ?>
 
+            <div class="shipping">
+                <h3 class="title-shipping">LIVRAISON</h3>
+                <?php do_action('woocommerce_checkout_shipping'); ?>
+            </div>
+
+            <div class="billing">
+                <h3 class="title-billing">FACTURATION</h3>
+                <?php do_action('woocommerce_checkout_billing'); ?>
+            </div>
+        </div>
+
+        <div class="buttons-ci">
+            <button class="button-archibald margin-bottom">Continuer</button>
+            <button class="button-archibald">Précédemment</button>
+        </div>
+    </div>
+</form>
 
 <?php get_footer(); ?>
